6b5099db25548a12ac307b96f209febf23a4fdd6
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2025-10-01 14:41:10 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (56.56%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int result = numBottles; // D
+        int emptyWB = numBottles;
+
+        while (emptyWB >= numExchange) {
+            // E
+            int newWB = emptyWB / numExchange; // 15 % 4 = 3
+            result += newWB;
+            emptyWB = newWB + (emptyWB % numExchange);
+        }
+
+        return result;
+    }
+}
