147cab3da5b6e0fe1b57bbe27f09f7e98364b73e
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2025-11-13 20:56:42 +0530

    Time: 7 ms (78.52%) | Memory: 47.5 MB (17.78%) - LeetSync

---
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxOperations(String s) {
+        int ones = 0, opr = 0;
+        for(int i = 0 ; i<s.length() ; i++) {
+            char c = s.charAt(i);
+            if(c == '1') {
+                ones += 1;
+            }else{
+                if(i > 0 && s.charAt(i - 1) == '1') {
+                    opr += ones;
+                }
+            }
+        }    
+        return opr;    
+    }
+}
