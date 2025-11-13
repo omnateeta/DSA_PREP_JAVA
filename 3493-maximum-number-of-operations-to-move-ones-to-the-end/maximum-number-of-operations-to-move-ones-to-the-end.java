a767ccbf2c756b2a67f7b121848173db25e5e819
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2025-11-13 22:06:30 +0530

    Update maximum-number-of-operations-to-move-ones-to-the-end.java

--- 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.java
+++ 3493-maximum-number-of-operations-to-move-ones-to-the-end/maximum-number-of-operations-to-move-ones-to-the-end.java
@@ -2,7 +2,7 @@ class Solution {
     public int maxOperations(String s) {
         int ones = 0, opr = 0;
         for(int i = 0 ; i<s.length() ; i++) {
-            char c = s.charAt(i);
+            char c = s.charAt(i); 
             if(c == '1') {
                 ones += 1;
             }else{
