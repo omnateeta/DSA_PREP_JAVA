48950d93d362388d61125e1fe315c02502da1d87
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2025-11-26 21:13:48 +0530

    Update binary-prefix-divisible-by-5.java

--- 1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.java
+++ 1071-binary-prefix-divisible-by-5/binary-prefix-divisible-by-5.java
@@ -2,7 +2,6 @@ class Solution {
     public List<Boolean> prefixesDivBy5(int[] nums) {
         List<Boolean> res = new ArrayList<>();
         int decimal = 0;
-
         for (int n : nums) {
             decimal = (2 * decimal + n) % 5;
             res.add(decimal % 5 == 0);
