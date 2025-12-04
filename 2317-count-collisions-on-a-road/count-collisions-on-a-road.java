802d92667f2c7c6d5a6db912365fcf7f11ae5492
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2025-12-04 18:37:57 +0530

    Time: 10 ms (93.15%) | Memory: 47.4 MB (39.11%) - LeetSync

---
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countCollisions(String directions) {
+        int n = directions.length();
+        int left = 0, right = n - 1;
+
+        while (left < n && directions.charAt(left) == 'L') {
+            left += 1;
+        }
+
+        while (right >= 0 && directions.charAt(right) == 'R') {
+            right -= 1;
+        }
+
+        int collisions = 0;
+        for (int i = left; i <= right; i++) {
+            if (directions.charAt(i) != 'S') {
+                collisions += 1;
+            }
+        }
+
+        return collisions;
+    }
+}
