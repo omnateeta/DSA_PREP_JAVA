dd202f649dc5433a39da3b0dbe1be01b2464587e
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2025-12-20 16:20:20 +0530

    Time: 8 ms (74.40%) | Memory: 46.8 MB (66.56%) - LeetSync

---
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minDeletionSize(String[] strs) {
+        // O(m*n)
+        int len = strs[0].length();
+        int ans = 0;
+
+        for(int i=0; i<len; i++) {
+            for(int j=1; j<strs.length; j++) {
+                if(strs[j].charAt(i) < strs[j-1].charAt(i)) {
+                    ans++;
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
