ad5a41513fce38dca495f09a444d7cd426a1dbe4
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2025-12-28 13:17:24 +0530

    Time: 1 ms (57.12%) | Memory: 46.8 MB (76.23%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.java
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.java
@@ -13,3 +13,48 @@ class Solution {
         return count;
     }
 }
+
+
+// Approach - 2 : // O(m * log(n)) -- Binary Search Optimal
+
+// class Solution {
+//     public int countNegatives(int[][] grid) {
+//         int m = grid.length, n = grid[0].length;
+//         int count = 0;
+
+//         for (int i = 0; i < m; i++) {
+//             int l = 0, r = n - 1;
+//             while (l <= r) {
+//                 int mid = l + (r - l) / 2;
+//                 if (grid[i][mid] < 0) {
+//                     r = mid - 1;
+//                 } else {
+//                     l = mid + 1;
+//                 }
+//             }
+//             // l is the first negative index
+//             count += (n - l);
+//         }
+//         return count;
+//     }
+// }
+
+// Approach - 3: // O(m + n) -- Super Optimal
+
+// class Solution {
+//     public int countNegatives(int[][] grid) {
+//         int m = grid.length, n = grid[0].length;
+//         int row = 0, col = n - 1;
+//         int count = 0;
+
+//         while (row < m && col >= 0) {
+//             if (grid[row][col] < 0) {
+//                 count += (m - row);
+//                 col--;
+//             } else {
+//                 row++;
+//             }
+//         }
+//         return count;
+//     }
+// }
