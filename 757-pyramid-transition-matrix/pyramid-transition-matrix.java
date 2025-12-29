1c56f0c5ae3d124ffa0274a7abbe4256c330506a
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2025-12-29 18:54:44 +0530

    Time: 1977 ms (7.65%) | Memory: 47 MB (54.78%) - LeetSync

---
+++ 757-pyramid-transition-matrix/pyramid-transition-matrix.java
@@ -0,0 +1,30 @@
+class Solution {
+    Map<String, List<Character>> hmap;
+    public boolean pyramidTransition(String bottom, List<String> allowed) {
+        hmap = new HashMap<>();
+        for(int i=0; i<allowed.size(); i++) {
+            String str = allowed.get(i);
+            String key = str.substring(0,2);
+            if(!hmap.containsKey(key)) {
+                hmap.put(key, new ArrayList<>());
+            }
+            hmap.get(key).add(str.charAt(2));
+        }
+        return helper(bottom, "", 0);
+    }
+    boolean helper(String row, String next, int index) {
+        if(row.length()==1)
+            return true; // went to the top of the pyramid
+        // if nextrow is just one less than current row, start again with this row
+        if(next.length()==row.length()-1) {
+            return helper(next, "", 0);
+        }    
+        String key = row.substring(index, index+2);
+        if(!hmap.containsKey(key))            return false; // we can't get to next row for this block
+        for(char ch : hmap.get(key)) {
+            if(helper(row, next+ch, index+1))
+                return true;
+        }
+        return false;
+    }
+}
