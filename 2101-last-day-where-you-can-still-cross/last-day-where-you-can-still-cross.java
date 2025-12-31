b891b2e341f7e4a8b241c8e05e85d6e1e8b79d09
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2025-12-31 11:28:58 +0530

    Time: 20 ms (95.43%) | Memory: 76.4 MB (81.73%) - LeetSync

---
+++ 2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.java
@@ -0,0 +1,73 @@
+class Solution {
+
+    private int[] parent, rank;
+    private int rows, cols;
+    private final int[][] DIRS = {{1,0},{-1,0},{0,1},{0,-1}};
+
+    public int latestDayToCross(int row, int col, int[][] cells) {
+        rows = row;
+        cols = col;
+
+        int total = row * col;
+        int TOP = total;
+        int BOTTOM = total + 1;
+
+        parent = new int[total + 2];
+        rank = new int[total + 2];
+
+        for (int i = 0; i < parent.length; i++)
+            parent[i] = i;
+
+        boolean[][] land = new boolean[row][col];
+
+        // Process in reverse order
+        for (int day = cells.length - 1; day >= 0; day--) {
+            int r = cells[day][0] - 1;
+            int c = cells[day][1] - 1;
+
+            land[r][c] = true;
+            int index = r * col + c;
+
+            // Connect neighbors
+            for (int[] d : DIRS) {
+                int nr = r + d[0];
+                int nc = c + d[1];
+                if (nr >= 0 && nr < row && nc >= 0 && nc < col && land[nr][nc]) {
+                    union(index, nr * col + nc);
+                }
+            }
+
+            // Connect to virtual nodes
+            if (r == 0) union(index, TOP);
+            if (r == row - 1) union(index, BOTTOM);
+
+            // Check connectivity
+            if (find(TOP) == find(BOTTOM))
+                return day;
+        }
+
+        return 0;
+    }
+
+    private int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    private void union(int x, int y) {
+        int rx = find(x);
+        int ry = find(y);
+
+        if (rx != ry) {
+            if (rank[rx] < rank[ry]) {
+                parent[rx] = ry;
+            } else if (rank[rx] > rank[ry]) {
+                parent[ry] = rx;
+            } else {
+                parent[ry] = rx;
+                rank[rx]++;
+            }
+        }
+    }
+}
