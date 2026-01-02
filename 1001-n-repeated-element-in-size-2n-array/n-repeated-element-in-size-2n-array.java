d834a1a957a0ac914dc4ba32959d9eacb387f757
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2026-01-02 17:43:42 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (12.74%) - LeetSync

---
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        int n = nums.length;
+        // Time: O(n)
+        // Space: O(1)
+        // Check only nearby elements.
+        // The repeated element must match within distance ≤ 3.
+        for(int i=0; i<n-1; i++) {
+            if(nums[i]==nums[i+1])
+                return nums[i];
+            if(i+2<n && nums[i]==nums[i+2])
+                return nums[i];
+            if(i+3<n && nums[i]==nums[i+3])
+                return nums[i];
+        }
+        return -1;
+    }
+}
+/* n unique, n numbers repeat
+// [9,6,0,9]
+// [1,0,2,3,9,9]
+hence there is the chance for repeated elements to show within three indices.
+We can check the adjacent three numbers for each index.
+*/
