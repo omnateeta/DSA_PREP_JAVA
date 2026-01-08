c57b7931129fd0a22a46085f0ed9b82b895d5893
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2026-01-08 19:03:41 +0530

    Time: 11 ms (59.26%) | Memory: 48.5 MB (14.81%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.java
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.java
@@ -26,3 +26,35 @@ class Solution {
 
     
 }
+
+
+/**************Iterative Dp******** */
+// class Solution {
+//     public int maxDotProduct(int[] nums1, int[] nums2) {
+//         int n = nums1.length;
+//         int m = nums2.length;
+
+//         int[][] dp = new int[n + 1][m + 1];
+
+//         // initialize base cases
+//         for (int i = 0; i <= n; i++) {
+//             for (int j = 0; j <= m; j++) {
+//                 dp[i][j] = Integer.MIN_VALUE;
+//             }
+//         }
+
+//         for (int i = n - 1; i >= 0; i--) {
+//             for (int j = m - 1; j >= 0; j--) {
+//                 int take = nums1[i] * nums2[j]
+//                          + Math.max(0, dp[i + 1][j + 1]);
+
+//                 int skip1 = dp[i + 1][j];
+//                 int skip2 = dp[i][j + 1];
+
+//                 dp[i][j] = Math.max(take, Math.max(skip1, skip2));
+//             }
+//         }
+
+//         return dp[0][0];
+//     }
+// }
