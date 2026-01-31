a57f37465960f7bad52b8740dad95cb08bc2b8dc
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2026-01-31 19:01:50 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (65.72%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.java
@@ -11,3 +11,23 @@ class Solution {
         return c;
     }
 }
+
+// Approach 2: //O(log n)
+
+// class Solution {
+//     public char nextGreatestLetter(char[] letters, char target) {
+//         int l = 0, r = letters.length - 1;
+//         char ans = letters[0];
+
+//         while (l <= r) {
+//             int mid = l + (r - l) / 2;
+//             if (letters[mid] > target) {
+//                 ans = letters[mid];
+//                 r = mid - 1;
+//             } else {
+//                 l = mid + 1;
+//             }
+//         }
+//         return ans;
+//     }
+// }
