d95d643748524144fbd2d665ee415e223b825e4a
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2026-02-10 21:22:30 +0530

    Update n-repeated-element-in-size-2n-array.java

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.java
@@ -7,7 +7,7 @@ class Solution {
         // The repeated element must match within distance ≤ 3.
         for(int i=0; i<n-1; i++) {
             if(nums[i]==nums[i+1])
-                return nums[i];
+                return nums[i]; 
             if(i+2<n && nums[i]==nums[i+2])
                 return nums[i];
             if(i+3<n && nums[i]==nums[i+3])
