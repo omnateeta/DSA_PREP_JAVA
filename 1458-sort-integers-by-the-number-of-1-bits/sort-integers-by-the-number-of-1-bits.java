604d510396bb970f290fdc7360c69c319b89cdc6
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2026-02-25 16:36:37 +0530

    Time: 9 ms (45.29%) | Memory: 46.8 MB (39.38%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
@@ -14,3 +14,33 @@ class Solution {
         return res;
     }
 }
+
+// Approach 2 : //O(nlogn)
+
+// class Solution {
+//     // Brian Kernighan's Algorithm
+//     private int countSetBits(int n) {
+//         int count = 0;
+
+//         while (n > 0) {
+//             n = n & (n - 1);   // remove last set bit
+//             count++;
+//         }
+
+//         return count;
+//     }
+//     public int[] sortByBits(int[] arr) {
+//         int n = arr.length;
+//         List<int[]> list = new ArrayList<>();
+//         for(int a : arr){
+//             int count = countSetBits(a);
+//             list.add(new int[]{a, count});
+//         }
+//         Collections.sort(list, (a,b) -> a[1] != b[1] ? a[1] - b[1] : a[0] -b[0]);
+//         int[] res = new int[n];
+//         for(int i=0;i<list.size();i++){
+//             res[i] = list.get(i)[0];
+//         }
+//         return res;
+//     }
+// }
