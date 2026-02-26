f63a9123908a111ffcc230862560e66d812f5505
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2026-02-26 19:11:24 +0530

    Time: 4 ms (21.83%) | Memory: 45.6 MB (18.29%) - LeetSync

---
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numSteps(String s) {
+        int count = 0;
+        StringBuilder sb = new StringBuilder(s);
+        
+        while (!sb.toString().equals("1")) {
+            int len = sb.length();
+            if (sb.charAt(len - 1) == '0') {
+                sb.deleteCharAt(len - 1);
+            } else {
+                int i = len - 1;
+                while (i >= 0 && sb.charAt(i) == '1') {
+                    sb.setCharAt(i, '0');
+                    i--;
+                }
+                if (i >= 0) {
+                    sb.setCharAt(i, '1');
+                } else {
+                    sb.insert(0, '1');
+                }
+            }
+            count++;
+        }
+        
+        return count;
+    }
+}
