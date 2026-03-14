5fb8614562df5dd7500710172c41c6eeb1287214
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2026-03-14 22:09:25 +0530

    Time: 6 ms (52.67%) | Memory: 45.1 MB (47.07%) - LeetSync

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    int count = 0;
+    String result = "";
+
+    public String getHappyString(int n, int k) {
+        backtrack("", n, k);
+        return result;
+    }
+
+    private void backtrack(String curr, int n, int k) {
+        if (curr.length() == n) {
+            count++;
+            if (count == k) {
+                result = curr;
+            }
+            return;
+        }
+
+        for (char ch : new char[]{'a', 'b', 'c'}) {
+            if (curr.length() > 0 && curr.charAt(curr.length() - 1) == ch) {
+                continue;
+            }
+
+            backtrack(curr + ch, n, k);
+
+            if (!result.equals("")) return;
+        }
+    }
+}
