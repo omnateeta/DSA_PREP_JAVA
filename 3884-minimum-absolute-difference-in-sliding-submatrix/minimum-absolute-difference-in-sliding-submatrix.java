4b130503d4de46f91648b61038608fad3d229da2
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2026-03-20 11:45:39 +0530

    Time: 26 ms (48.94%) | Memory: 47.6 MB (68.09%) - LeetSync

--- 3884-minimum-absolute-difference-in-sliding-submatrix/minimum-absolute-difference-in-sliding-submatrix.java
+++ 3884-minimum-absolute-difference-in-sliding-submatrix/minimum-absolute-difference-in-sliding-submatrix.java
@@ -4,19 +4,18 @@ class Solution {
         int ans[][] = new int[m-k+1][n-k+1];
 
         // (m-k)(n-k)*(k*k + k*k*logk)
-
         for(int i=0; i<m-k+1; i++) {
             for(int j=0; j<n-k+1; j++) {
 
                 TreeSet<Integer> tset = new TreeSet<>();
                 for(int r=i; r<i+k; r++) {
                     for(int c=j; c<j+k; c++) {
-                        tset.add(grid[r][c]);
+                        tset.add(grid[r][c]); 
                     }
                 }
 
                 int min = Integer.MAX_VALUE;
-                // 1 2 3 4 5
+                // 1 2 3 4 5 
                 int prev = tset.first();
                 for(int val : tset) {
                     if(val!=prev) {
