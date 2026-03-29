ec3a7c44a38d24e4f2b681705edee238cbc272df
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2026-03-29 13:52:56 +0530

    Time: 1 ms (100.00%) | Memory: 44.1 MB (68.88%) - LeetSync

---
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean canBeEqual(String s1, String s2) {
+        for(int i=0;i<4;i++){
+            if(s1.charAt(i) != s2.charAt(i) && s1.charAt(i) != s2.charAt((i+2) % 4)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
