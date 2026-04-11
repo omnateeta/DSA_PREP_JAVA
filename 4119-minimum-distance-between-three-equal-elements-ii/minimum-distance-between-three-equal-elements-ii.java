d5a8a55c809fcf73c38f24449c8b87ba8f1dfb4d
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2026-04-11 16:07:35 +0530

    Time: 134 ms (35.75%) | Memory: 277.3 MB (11.17%) - LeetSync

---
+++ 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minimumDistance(int[] nums) {
+        HashMap<Integer, List<Integer>> hmap = new HashMap<>();
+        for(int i=0; i<nums.length; i++) {
+            hmap.putIfAbsent(nums[i], new ArrayList<>());
+            hmap.get(nums[i]).add(i);
+        }
+        int dist = Integer.MAX_VALUE;
+
+        for(int key : hmap.keySet()) {
+            if(hmap.get(key).size() < 3)
+                continue;
+            List<Integer> temp  = hmap.get(key);
+            for(int i=0; i<temp.size()-2; i++) {
+                int x = temp.get(i+1)-temp.get(i);
+                int y = temp.get(i+2)-temp.get(i);
+                int z = temp.get(i+2)- temp.get(i+1);
+                dist = Math.min(dist, x+y+z);
+            }
+        }
+        return dist==Integer.MAX_VALUE ? -1 : dist;
+        
+    }
+}
