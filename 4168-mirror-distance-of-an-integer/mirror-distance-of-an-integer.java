4a15d10c72d625fa2b4c50495b8feb22e0fe75e2
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2026-04-18 21:24:36 +0530

    Time: 1 ms (100.00%) | Memory: 42.5 MB (65.56%) - LeetSync

---
+++ 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.java
@@ -0,0 +1,14 @@
+//3783. Mirror Distance of an Integer..
+class Solution {
+    public int mirrorDistance(int n) {
+        int original = n;
+        int rev = 0;
+        // Reverse the number
+        while (n > 0) {
+            int digit = n % 10;
+            rev = rev * 10 + digit;
+            n /= 10;
+        }
+        return Math.abs(original - rev);
+    }
+}
