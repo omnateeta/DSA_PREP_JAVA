62cce7cef62a3e3306e5163eedf018b31bc2f31a
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2026-05-04 18:08:53 +0530

    Create 48. Rotate Image.java

---
+++ 48. Rotate Image.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // Transpose the matrix
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < i; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Reverse each row
+        for(int i = 0; i < n; i++) {
+            int j = 0, k = n - 1;
+            while(j < k) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][k];
+                matrix[i][k] = temp;
+                j++;
+                k--;
+            }
+        }
+    }
+}
