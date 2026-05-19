33384b54837a96180656797d9e96829645b72c90
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2026-05-19 18:10:26 +0530

    Update 2540. Minimum Common Value.java

--- 2540. Minimum Common Value.java
+++ 2540. Minimum Common Value.java
@@ -16,11 +16,8 @@ class Solution {
 }
 
 /*
-
 [1,2,3,6]
 [2,3,4,5,8,9]
-
-
 Given two integer arrays nums1 and nums2, sorted in non-decreasing order, return the minimum integer common to both arrays. If there is no common integer amongst nums1 and nums2, return -1.
 
 Note that an integer is said to be common to nums1 and nums2 if both arrays have at least one occurrence of that integer.
