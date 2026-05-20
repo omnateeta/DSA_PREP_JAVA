3cbb66937e75b6e07f91578d83fba86244a06f2c
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2026-05-20 11:36:40 +0530

    Update 2657. Find the Prefix Common Array of Two Arrays.java

--- 2657. Find the Prefix Common Array of Two Arrays.java
+++ 2657. Find the Prefix Common Array of Two Arrays.java
@@ -29,7 +29,7 @@ class Solution {
     }
 }
 
-/*
+/* Problem descreption
 You are given two 0-indexed integer permutations A and B of length n.
 
 A prefix common array of A and B is an array C such that C[i] is equal to the count of numbers that are present at or before the index i in both A and B.
