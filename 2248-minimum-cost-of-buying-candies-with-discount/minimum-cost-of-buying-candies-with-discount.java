fcf4fb0f789ee06805a364d35bf38a73e8b472cb
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2026-06-01 21:26:00 +0530

    Time: 6 ms (41.78%) | Memory: 44.5 MB (51.02%) - LeetSync

--- 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.java
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.java
@@ -1,18 +1,18 @@
 //2144. Minimum Cost of Buying Candies With Discount
-class Solution {
+class Solution { 
     public int minimumCost(int[] cost) {
         Arrays.sort(cost);
-        int ans = 0;
-        int taken = 0;
+        int ans = 0; 
+        int taken = 0; 
 
         for(int i=cost.length-1; i>=0; i--) {
             if(taken==2) {
-                taken = 0;
-            } else {
+                taken = 0; 
+            } else { 
                 ans += cost[i];
                 taken++;
             }
         }
         return ans;
     }
-}
+} // End of the code
