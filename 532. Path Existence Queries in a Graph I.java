51336ece61e5fef2ad2d10b17a7212b933467927
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2026-07-09 21:18:25 +0530

    Update 532. Path Existence Queries in a Graph I.java

--- 532. Path Existence Queries in a Graph I.java
+++ 532. Path Existence Queries in a Graph I.java
@@ -1,1 +1,25 @@
+//3532. Path Existence Queries in a Graph I
+class Solution {
+    public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
+        int[] component = new int[n];
+        int id = 0;
+        component[0] = 0;
 
+        for (int i = 1; i < n; i++) {
+            if (nums[i] - nums[i - 1] > maxDiff) {
+                id++;
+            }
+            component[i] = id;
+        }
+
+        boolean[] ans = new boolean[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+            int u = queries[i][0];
+            int v = queries[i][1];
+            ans[i] = (component[u] == component[v]);
+        }
+
+        return ans;
+    }
+}
