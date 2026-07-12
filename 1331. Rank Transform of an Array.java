394946e1db10fea02c622885df1bf84ea351ebc0
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2026-07-12 20:17:10 +0530

    Add rank transformation for an array
    Implement array rank transformation using sorting and a hashmap.

--- 1331. Rank Transform of an Array.java
+++ 1331. Rank Transform of an Array.java
@@ -1,1 +1,23 @@
+import java.util.*;
 
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int[] sorted = arr.clone();
+        Arrays.sort(sorted);
+
+        HashMap<Integer, Integer> rank = new HashMap<>();
+        int r = 1;
+
+        for (int num : sorted) {
+            if (!rank.containsKey(num)) {
+                rank.put(num, r++);
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rank.get(arr[i]);
+        }
+
+        return arr;
+    }
+}
