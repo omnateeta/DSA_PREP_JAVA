4b2a0e26b700d8e711d7281a51593c1f869cff87
Omnateeta V Unnimath <160630589+omnateeta@users.noreply.github.com>
2026-07-15 14:41:32 +0530

    Implement gcdOfOddEvenSums method in Solution class

--- 3658. GCD of Odd and Even Sums.py
+++ 3658. GCD of Odd and Even Sums.py
@@ -1,1 +1,8 @@
-
+//3658. GCD of Odd and Even Sums 
+class Solution(object):
+    def gcdOfOddEvenSums(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return n
